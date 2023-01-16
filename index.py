89f87ed5d52bcc0c5f7da0cef65257cac560f6a7
Parid V <49834648+ParidV@users.noreply.github.com>
2023-01-16 12:24:48 +0100

    Added the mail service and fixed error handeling

--- index.py
+++ index.py
@@ -1,9 +1,11 @@
 from flask import Flask, request, jsonify
 import requests
 from flask_cors import CORS
+import smtplib
 
 app = Flask(__name__)
 CORS(app)
+
 
 @app.route("/register", methods=["POST"])
 def register():
@@ -15,8 +17,8 @@ def register():
 
     if not all([firstName, lastName, email, username, password]):
         return jsonify(success=False, error="Inputs are missing"), 400
-
     try:
+        # Step 1: Get access token
         token_request = requests.post(
             "http://localhost:8080/realms/keycloak-react-auth/protocol/openid-connect/token",
             data={
@@ -33,6 +35,7 @@ def register():
         token_request.raise_for_status()
         access_token = token_request.json()["access_token"]
 
+        # Step 2: Create user
         create_user_request = requests.post(
             "http://localhost:8080/admin/realms/keycloak-react-auth/users",
             json={
@@ -56,14 +59,36 @@ def register():
         )
 
         create_user_request.raise_for_status()
+
+        # Step 3: Send email
+        # create an SMTP object
+        # server = smtplib.SMTP('smtp.gmail.com', 587)
+
+        # # start the encryption
+        # server.starttls()
+
+        # # login to your email account
+        # server.login("your_email@gmail.com", "your_password")
+
+        # # send the email
+        # msg = "Hello, this is a test email."
+        # server.sendmail("your_email@gmail.com", "recipient_email@example.com", msg)
+
+        # # end the SMTP session
+        # server.quit()
+
         return jsonify(success=True, message="User created successfully")
     except requests.exceptions.HTTPError as err:
+        print(err.response.json())
+
         if err.response.status_code == 409:
-            return jsonify(success=False, error="Users Exists"), 409
+            errorText = err.response.json()["errorMessage"]
+            return jsonify(success=False, error=errorText), 409
         else:
             return jsonify(success=False, error="Server Error"), 500
     except Exception as err:
         return jsonify(success=False, error=str(err)), 500
+
 
 if __name__ == '__main__':
     app.run(port=4500)
