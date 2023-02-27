95fee03668038adab6fdf30aee2cdfe6bc8edc1f
paridv <pvaroshi@gmail.com>
2023-02-27 11:20:27 +0000

    Adding more columns in database

--- initial_queries.sql
+++ initial_queries.sql
@@ -7,7 +7,22 @@ CREATE TABLE user_registered (
   email varchar(255) NOT NULL,
   token varchar(255) NOT NULL,
   isVerified boolean DEFAULT false,
+	keycloak_user_creation boolean DEFAULT false,
+  keycloak_generate_client boolean DEFAULT false,
+  keycloak_create_roles_for_client boolean DEFAULT false,
+  keycloak_role_mapping boolean DEFAULT false,
+  yml_genereration boolean DEFAULT false,
+  frost_yml_execution boolean DEFAULT false,
+  node_red_command_execution boolean DEFAULT false,
+  node_red_install_libaries boolean DEFAULT false,
+  node_red_keycloak_generate_new_client boolean DEFAULT false,
+  node_red_keycloak_generate_roles boolean DEFAULT false,
+  node_red_keycloak_role_mapping boolean DEFAULT false,
+  node_red_replace_settings boolean DEFAULT false,
+  node_red_restart_container boolean DEFAULT false,
   isCompleted boolean DEFAULT false,
   createdAt timestamp DEFAULT CURRENT_TIMESTAMP,
   completedAt timestamp DEFAULT NULL
 );
+
+
